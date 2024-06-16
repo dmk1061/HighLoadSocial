8f3f3809572886b50fed00ee8e2cacd139fae133
prbinom@mail.ru <prbinom@mail.ru>
2024-06-16 20:27:55 +0700

    haproxy log

--- prometheus/test.sh
+++ prometheus/test.sh
@@ -4,6 +4,6 @@ count=1  # Начальное количество запусков
 
 while true; do
     echo "Iteration $count"
-    ab -n 6000 -c 10 -t 600 http://localhost:80/dialog/get/2
+    ab -n 6000 -c 1 -t 600 http://localhost/dialog/get/2
     ((count++))  # Увеличение счетчика на 1
 done
